e9db741f3b840bbd4aa5db67eddc106e909c0c40
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-09-27 21:46:53 +0200

    Modify TimerChallenge component Add handleStop function Use useRef to store timeout registry immune to component reload

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const TimerChallenge = ({ title, targetTime }) => {
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
+  const timer = useRef();
+
   const handleStart = () => {
     setTimerStarted(true);
 
-    setTimeout(() => setTimerExpired(true), targetTime * 1000);
+    timer.current = setTimeout(() => setTimerExpired(true), targetTime * 1000);
+  };
+
+  const handleStop = () => {
+    clearTimeout(timer.current);
   };
 
   return (
@@ -18,7 +24,7 @@ const TimerChallenge = ({ title, targetTime }) => {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
