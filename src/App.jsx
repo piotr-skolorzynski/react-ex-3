f8bd31c5d2601d6aa4840d623e1d6c6c8717fe19
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-09-27 18:42:44 +0200

    Add two way binding in Player component to let user name change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
 
-function App() {
+const App = () => {
   return (
     <>
       <Player />
       <div id="challenges"></div>
     </>
   );
-}
+};
 
 export default App;
