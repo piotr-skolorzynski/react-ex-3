1853fe5191916355a8d92663af61e9c02db589d9
piotr-skolorzynski <p.skolorzynski@gmail.com>
2025-09-29 08:20:50 +0200

    Display player result

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -9,7 +9,7 @@ const ResultModal = forwardRef(function ResultModal(
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
-  console.log(formattedRemainingTime);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -21,9 +21,9 @@ const ResultModal = forwardRef(function ResultModal(
 
   return (
     <dialog ref={dialog} className="result-modal">
-      {userLost && <h2>You lost</h2>}
+      {userLost ? <h2>You lost</h2> : <h2>Your score: {score}</h2>}
       <p>
-        The target time was <strong>{targetTime}seconds.</strong>
+        The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
         You stopped the timer with{" "}
